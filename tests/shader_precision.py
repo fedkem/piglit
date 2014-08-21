fe3be70f12c4df6c066b950d8085fe926f8a4e18
Micah Fedke <micah.fedke@collabora.co.uk>
2014-08-21 08:56:39 -0500

    arb_shader_precision: adding log2

--- tests/shader_precision.py
+++ tests/shader_precision.py
@@ -24,3 +24,4 @@ profile.tests['spec/arb_shader_precision-gte'] = PiglitTest('arb_shader_precisio
 profile.tests['spec/arb_shader_precision-pow'] = PiglitTest('arb_shader_precision-pow')
 profile.tests['spec/arb_shader_precision-exp'] = PiglitTest('arb_shader_precision-exp')
 profile.tests['spec/arb_shader_precision-exp2'] = PiglitTest('arb_shader_precision-exp2')
+profile.tests['spec/arb_shader_precision-log2'] = PiglitTest('arb_shader_precision-log2')
